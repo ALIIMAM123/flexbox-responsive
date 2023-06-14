3189d868b5398b58f091d07e037d16d7aff9982a
Md.Ali imam <86283374+ALIIMAM123@users.noreply.github.com>
2023-06-14 12:52:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ul className="ul_container">
+        <li className="li-items">1</li>
+        <li className="li-items">2</li>
+        <li className="li-items">3</li>
+        <li className="li-items">4</li>
+        <li className="li-items">1</li>
+        <li className="li-items">2</li>
+        <li className="li-items">3</li>
+        <li className="li-items">4</li>
+      </ul>
+    </div>
+  );
+}
